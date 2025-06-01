d1f85985002982976af13aa7c56076124f4d2415
mcapriotti1 <michaelcapriotti11@gmail.com>
2025-06-01 15:37:19 -0500

    Home Page 50% done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,22 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <nav className="bg-primary text-maintext font-heading font-bold sticky top-0 z-50 shadow-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-20 sm:h-24 md:h-24 lg:h-32 xl:h-32">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-20 sm:h-24 md:h-24 lg:h-24 xl:h-24">
           {/* Logo */}
           <Link
             to="/"
-            className="
-              flex items-center space-x-2
-              pt-2 sm:pt-4 md:pt-6 lg:pt-8 xl:pt-8
-            "
           >
             <img
-              src="/images/Hudson_Logo.png"
+              src="/images/Hudson_Logo.jpeg"
               alt="Home"
               className="
-                h-[150px] sm:h-[190px]
-                md:h-[210px] lg:h-[250px] xl:h-[300px]
+                h-[70px] sm:h-[85px]
+                md:h-[85px] lg:h-[110px] xl:h-[120px]
                 w-auto
+                border border-transparent hover:border-white
+                shadow-md hover:shadow-lg
+                transition-colors transition-shadow duration-1000
               "
             />
           </Link>
@@ -28,28 +27,13 @@ export default function Header() {
           {/* Menu */}
           <ul className="flex space-x-6 items-center text-sm sm:text-base md:text-lg lg:text-xl">
           <li>
-            <Link
-              to="/shop"
-              className="px-3 py-1 rounded hover:bg-black/20 hover:text-buttonhover transition duration-300"
-            >
-              Shop All
-            </Link>
+            <Link to="/shop" className="underline-hover">Shop All</Link>
           </li>
           <li>
-            <Link
-              to="/checkout"
-              className="px-3 py-1 rounded hover:bg-black/20 hover:text-buttonhover transition duration-300"
-            >
-              Checkout
-            </Link>
+            <Link to="/checkout" className="underline-hover">Checkout</Link>
           </li>
           <li>
-            <Link
-              to="/about"
-              className="px-3 py-1 rounded hover:bg-black/20 hover:text-buttonhover transition duration-300"
-            >
-              About
-            </Link>
+            <Link to="/about" className="underline-hover">About</Link>
           </li>
         </ul>
         </div>
